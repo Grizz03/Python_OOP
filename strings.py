9275a23163b8078486abe8c5694709a4bfceffcf
Grizz03 <steve.grizz03@gmail.com>
2021-05-25 16:57:03 -0700

    Convert string to UNICODE

---
+++ strings.py
@@ -0,0 +1,13 @@
+norm_string = input('Enter a string to convert to UNICODE: ')
+secret_string = ''
+
+for char in norm_string:
+    secret_string += str(ord(char) - 23)  # Converts string to UNICODE
+print('Secret Message: ', secret_string)
+norm_string = ''
+
+for x in range(0, len(secret_string)-1, 2):
+    char_code = secret_string[x] + secret_string[x+1]
+    norm_string += chr(int(char_code) + 23)  # Converts UNICODE to string
+
+print("Original Message: ", norm_string)
