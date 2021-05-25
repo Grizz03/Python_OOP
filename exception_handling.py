0f6a9ef3fbf399ba00f3c0c4cc0e14b545491746
Grizz03 <steve.grizz03@gmail.com>
2021-05-25 16:27:15 -0700

    number guessing with break and do while loop

---
+++ exception_handling.py
@@ -0,0 +1,20 @@
+# while True:  # continue until break is reached.
+#     try:
+#         number = int(input('Please enter a number: '))
+#         break  # will only break if number entered
+#     except ValueError:
+#         print("You didn't enter a number")
+#     except:
+#         print('An unknown error occurred')
+#
+# print('Thank you for entering a number')
+
+import random
+
+secret_number = random.randrange(1, 11)
+
+while True:
+    guess = int(input('Guess a number between 1 and 10: '))
+    if guess == secret_number:
+        print('You guessed the number!')
+        break
