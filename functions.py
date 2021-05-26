f39d0633762e50256d9d01b0e509f8f83407640c
Grizz03 <steve.grizz03@gmail.com>
2021-05-25 21:26:35 -0700

    area of shapes app functions defined

--- functions.py
+++ functions.py
@@ -1,14 +1,18 @@
+import math
+
+
 def mult_divide(num1, num2):
     return (num1 * num2), (num1 / num2)
 
 
 mult, divide = mult_divide(5, 4)
 
-print("5 * 4 =", mult)
-print("5 / 4 =", divide)
+
+# print("5 * 4 =", mult)
+# print("5 / 4 =", divide)
 
 
-# list of primes app
+# LIST OF PRIMES APP -->
 
 def is_prime(num):
     for x in range(2, num):
@@ -25,8 +29,53 @@ def get_primes(max_number):
     return list_of_primes
 
 
-max_num_to_check = int(input('Search for Primes up to: '))
+# max_num_to_check = int(input('Search for Primes up to: '))
 
-list_of_primes = get_primes(max_num_to_check)
-for prime in list_of_primes:
-    print(prime)
+# list_of_primes = get_primes(max_num_to_check)
+# for prime in list_of_primes:
+# print(prime)
+
+
+# SUM OF ALL WITH *ARGS -->
+
+def sum_all(*args):  # -> with args you can have multiple params <-
+    sum_1 = 0
+    for x in args:
+        sum_1 += x
+    return sum_1
+
+
+print('Sum: ', sum_all(1, 2, 3, 4, 5))  # input as many params as you want
+
+
+# AREA OF SHAPES APP -->
+
+def get_area(shape):
+    shape = shape.lower()
+    if shape == 'rectangle':
+        rectangle_area()
+    elif shape == 'circle':
+        circle_area()
+    else:
+        print('Please enter rectangle or circle..')
+
+
+def rectangle_area():
+    length = float(input('Enter the length: '))
+    width = float(input('Enter the width: '))
+    area = length * width
+    print('The area of the rectangle is ', area)
+
+
+def circle_area():
+    radius = float(input('Enter the radius: '))
+    area = math.pi * (math.pow(radius, 2))
+    print('the area of the circle is {:.2f}'.format(area))
+
+
+def main():
+    shape_type = input('Get area for what shape: ')
+    get_area(shape_type)
+
+
+main()
